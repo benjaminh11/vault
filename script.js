ae2b932091ccffb470204468f0b1b0ff0a2afaf0
bEN <benhernowitz@gmail.com>
2024-12-11 14:31:28 -0500

    main

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const msg = // first step, create a string with our message//
 const num1 = 5 + 5; //next step, creat variables that represent the numbers for our combination //
 const num2 = 20 * 2;
 const num3 = 50 - 11;
-const strComplete = `${msg} ${num1} ${num2} ${num3}`; //create a variable combining our message and combo//
+const strComplete = `${msg} ${num1} - ${num2} - ${num3}`; //create a variable combining our message and combo//
 alert(strComplete); //create alert box that contains our combined variable//
