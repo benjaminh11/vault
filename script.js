3818d76888e0b0eeacdd20f6434a60509880e119
bEN <benhernowitz@gmail.com>
2024-12-11 14:28:00 -0500

    main

---
+++ script.js
@@ -0,0 +1,7 @@
+const msg = // first step, create a string with our message//
+  " You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+const num1 = 5 + 5; //next step, creat variables that represent the numbers for our combination //
+const num2 = 20 * 2;
+const num3 = 50 - 11;
+const strComplete = `${msg} ${num1} ${num2} ${num3}`; //create a variable combining our message and combo//
+alert(strComplete); //create alert box that contains our combined variable//
